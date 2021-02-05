ef3cfe384e7626ed1c4c3fa725de3932433f1ee4
danajackson2 <danajackson2@gmail.com>
2021-02-05 11:22:04 -0800

    wow vanilla JS inside JSX is...

--- src/components/SimplerComponent.js
+++ src/components/SimplerComponent.js
@@ -1,1 +1,8 @@
-// Code SimplerComponent Here
+import React from 'react'
+
+const SimplerComponent = (props) => {
+    return(<div id='hi' onClick={props.handleClick}>I am just happy</div>)
+
+}
+
+export default SimplerComponent
